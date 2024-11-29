a1e1d8eb576da7db704c6f3836274580c9f98ba3
VENKATARAMANA BARATAM <109843726+Venkataramanabaratam1@users.noreply.github.com>
2024-11-29 23:34:10 +0530

    Update authController.js

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -14,18 +14,15 @@ owasp.config({
 
 
 export const register = async (req, res) => {
-    const { name, email, password } = req.body;
-
-    // Input validation
+    const { name, email, password, role } = req.body;
+    console.log(req.body);
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
-        console.log(errors.array());
         return res.status(400).json({ errors: errors.array() });
     }
 
     const passwordTest = owasp.test(password);
     if (!passwordTest.strong) {
-        console.log(passwordTest.errors);
         return res.status(400).json({ errors: passwordTest.errors });
     }
 
@@ -36,12 +33,13 @@ export const register = async (req, res) => {
 
     try {
         const hashedPassword = await bcrypt.hash(password, 10);
-        const user = await User.create({ name, email, password: hashedPassword });
+        const user = await User.create({ name, email, password: hashedPassword, role });
         res.status(201).json({ message: 'User registered successfully', user });
     } catch (err) {
         res.status(500).json({ message: 'Error registering user', error: err.message });
     }
 };
+
 
 export const login = async (req, res) => {
     const { email, password } = req.body;
@@ -136,7 +134,4 @@ Thankyou for joining!`,
         res.status(500).json({ message: 'Error adding role', error: err.message });
     }
 };
-
-
-
 
