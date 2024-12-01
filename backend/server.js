d315857a5a2dd16e0af8b161eda8ce1491172cbf
VENKATARAMANA BARATAM <109843726+Venkataramanabaratam1@users.noreply.github.com>
2024-12-01 21:50:39 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -13,49 +13,28 @@ const app = express();
 // Database connection
 connectDB();
 
-// app.use((req, res, next) => {
-//     res.set({
-//         "Access-Control-Allow-Origin": "*",
-//         "Access-Control-Allow-Methods": "*",
-//         "Access-Control-Allow-Headers": "'Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token'",
-//     });
-
-//     next();
-// });
-
 // Middleware
 app.use(express.json());
 app.use(cookieParser());
 app.use(helmet());
-
-// CORS configuration
-const corsOptions = {
-    origin: ["http://localhost:3000", "https://rbac-front-eight.vercel.app/"], // Frontend URL from environment variable or default
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-    credentials: true // Enable cookies/auth headers
-};
-app.use(cors(corsOptions)); // Apply CORS middleware
-app.options("*", cors(corsOptions)); // Handle preflight requests
+app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
 
 // Rate limiter for all routes
 app.use(rateLimiter);
 
-// Routes
 app.use('/api/auth', authRoutes);
 
-// Root route
-app.use('/', (req, res) => {
-    res.json({ message: "API is running" });
+app.use('/', (req,res) => {
+  res.json({message:"API IS RUNNING"})
 });
 
-// Error handling middleware
+// Error handling
 app.use((err, req, res, next) => {
     res.status(err.status || 500).json({ error: err.message || 'Server Error' });
 });
 
-// Server setup
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
