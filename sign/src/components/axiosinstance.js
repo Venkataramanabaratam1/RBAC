669910271efad9ea70b8d77663c06949c7a3ff49
VENKATARAMANA BARATAM <109843726+Venkataramanabaratam1@users.noreply.github.com>
2024-12-01 21:56:10 +0530

    Update axiosinstance.js

--- sign/src/components/axiosinstance.js
+++ sign/src/components/axiosinstance.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-    baseURL: process.env.REACT_APP_BACKENDURL,
+    baseURL:'https://rbac-backend-eight.vercel.app/api',
     withCredentials: true,
     headers: {
         'Content-Type': 'application/json',
