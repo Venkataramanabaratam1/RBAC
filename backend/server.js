db9d8b12337dc1333c592d59510afe2de81b55c6
VENKATARAMANA BARATAM <109843726+Venkataramanabaratam1@users.noreply.github.com>
2024-12-01 18:54:39 +0530

    Update server.js okay

--- backend/server.js
+++ backend/server.js
@@ -30,7 +30,7 @@ app.use(helmet());
 
 // CORS configuration
 const corsOptions = {
-    origin: process.env.CLIENT_URL || "https://rbac-front-jet.vercel.app", // Frontend URL from environment variable or default
+    origin: process.env.CLIENT_URL || "https://rbac-front-eight.vercel.app/", // Frontend URL from environment variable or default
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true // Enable cookies/auth headers
